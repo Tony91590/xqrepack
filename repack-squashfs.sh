d5eabe8626dab59f44957b702b8d04149295e1af
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-03-14 10:41:17 +0100

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -6,28 +6,7 @@
 # 2020.07.20  darell tan
 # 
 
-set -e
-
-IMG=$1
-ROOTPW='$1$qtLLI4cm$c0v3yxzYPI46s28rbAYG//'  # "password"
-
-[ -e "$IMG" ] || { echo "rootfs img not found $IMG"; exit 1; }
-
-# verify programs exist
-command -v unsquashfs &>/dev/null || { echo "install unsquashfs"; exit 1; }
-mksquashfs -version >/dev/null || { echo "install mksquashfs"; exit 1; }
-
-FSDIR=`mktemp -d /tmp/resquash-rootfs.XXXXX`
-trap "rm -rf $FSDIR" EXIT
-
-# test mknod privileges
-mknod "$FSDIR/foo" c 0 0 2>/dev/null || { echo "need to be run with fakeroot"; exit 1; }
-rm -f "$FSDIR/foo"
-
->&2 echo "unpacking squashfs..."
-unsquashfs -f -d "$FSDIR" "$IMG"
-
->&2 echo "patching squashfs..."
+FSDIR=`/mnt/rootfs/ubi`
 
 # create /opt dir
 mkdir "$FSDIR/opt"
@@ -36,9 +15,6 @@ chmod 755 "$FSDIR/opt"
 # modify dropbear init
 sed -i 's/channel=.*/channel=release2/' "$FSDIR/etc/init.d/dropbear"
 sed -i 's/flg_ssh=.*/flg_ssh=1/' "$FSDIR/etc/init.d/dropbear"
-
-# mark web footer so that users can confirm the right version has been flashed
-sed -i 's/romVersion%>/& xqrepack-adriano/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
 
 # stop resetting root password
 sed -i '/set_user(/a return 0' "$FSDIR/etc/init.d/system"
@@ -103,7 +79,3 @@ sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
 # apply patch from xqrepack repository
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
 find patches -type f -name \*.orig -delete
-
->&2 echo "repacking squashfs..."
-rm -f "$IMG.new"
-mksquashfs "$FSDIR" "$IMG.new" -comp xz -b 256K -no-xattrs
