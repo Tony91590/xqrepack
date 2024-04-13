488cb63110f6b64e6f357b39d27288caa768e0a4
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-04-13 23:10:01 +0200

    Update ubinize.sh

--- ubinize.sh
+++ ubinize.sh
@@ -58,7 +58,7 @@ vol_name=rootfs_data
 vol_flags=autoresize
 CFGEND2
 
-ubinize -o "$OUTPUT" -m 2048 -p 128KiB -O 2048 "$UBICFG"
+sudo ubinize -o "$OUTPUT" -m 2048 -p 128KiB -O 2048 "$UBICFG"
 
 echo "done."
 
