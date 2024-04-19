78fa2699dc12023c93aecf6203583288979f4d6b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-04-19 10:31:20 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -88,6 +88,11 @@ NVRAM
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
 find patches -type f -name \*.orig -delete
 
+
+rm -f $FSDIR/lib/wifi/qcawificfg80211.sh.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/setting/wifi.htm.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm.orig
 rm -f $FSDIR/etc/shadow.orig
 rm -f $FSDIR/usr/share/xiaoqiang/xiaoqiang_version.orig
 
