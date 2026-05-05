373d166be191d093cf3594c7821649cfc01635cd
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-05-06 00:55:58 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -35,8 +35,11 @@ sed -i '/vap_contryie/d' lib/wifi/qcawificfg80211.sh
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
 find patches -type f -name \*.orig -delete
 
+rm -f $FSDIR/lib/wifi/qcawificfg80211.sh.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/setting/wifi.htm.orig
 rm -f $FSDIR/etc/init.d/dropbear.orig
-rm -f $FSDIR/etc/shadow.orig
 
 >&2 echo "repacking squashfs..."
 rm -f "$IMG.new"
