bde05d94565401937f51776e19fe487ab8c7cf9b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-05-09 19:57:08 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -104,6 +104,16 @@ sed -i "s/\$country_code/FR/g" $FSDIR/lib/wifi/qcawificfg80211.sh
 sed -i 's/country_code="SG"/country_code="FR"/g' $FSDIR/lib/wifi/qcawificfg80211.sh
 sed -i 's/156/250/g' $FSDIR/lib/wifi/qcawificfg80211.sh
 sed -i 's/cfg80211/cfg80211 ieee80211_regdom=FR/g' $FSDIR/etc/modules.d/30-cfg80211-linux
+sed -i '/exit 0/i\
+iw reg set FR\
+for i in $(uci show wireless 2>/dev/null | grep "=wifi-iface" | cut -d. -f2 | cut -d= -f1); do\
+    uci set wireless.$i.ieee80211k=1\
+    uci set wireless.$i.ieee80211v=1\
+    uci set wireless.$i.bss_transition=1\
+done\
+uci commit wireless\
+wifi reload
+' $FSDIR/etc/rc.local
 
 # apply patch from xqrepack repository
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
