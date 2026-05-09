83e1ef44408b4f5aeae58f20a87d0d297ab84d5d
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-05-09 19:59:46 +0200

    Change country code to 'FR' in configuration
    Modify country code to 'FR' and update wireless settings.

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -105,6 +105,7 @@ sed -i 's/country_code="SG"/country_code="FR"/g' $FSDIR/lib/wifi/qcawificfg80211
 sed -i 's/156/250/g' $FSDIR/lib/wifi/qcawificfg80211.sh
 sed -i 's/cfg80211/cfg80211 ieee80211_regdom=FR/g' $FSDIR/etc/modules.d/30-cfg80211-linux
 sed -i '/exit 0/i\
+( sleep 15\
 iw reg set FR\
 for i in $(uci show wireless 2>/dev/null | grep "=wifi-iface" | cut -d. -f2 | cut -d= -f1); do\
     uci set wireless.$i.ieee80211k=1\
@@ -112,8 +113,8 @@ for i in $(uci show wireless 2>/dev/null | grep "=wifi-iface" | cut -d. -f2 | cu
     uci set wireless.$i.bss_transition=1\
 done\
 uci commit wireless\
-wifi reload
-' $FSDIR/etc/rc.local
+wifi reload ) &\
+' "$FSDIR/etc/rc.local"
 
 # apply patch from xqrepack repository
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
