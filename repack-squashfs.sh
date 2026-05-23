9afe23f1c6128abe923450a897c1c398c0350554
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-05-24 01:16:45 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -29,13 +29,7 @@ unsquashfs -f -d "$FSDIR" "$IMG"
 
 >&2 echo "patching squashfs..."
 
-# apply patch from xqrepack repository
-find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
-find patches -type f -name \*.orig -delete
 
-rm -f $FSDIR/usr/lib/lua/luci/model/cbi/firewall/zones.lua.orig
-rm -f $FSDIR/etc/opkg/distfeeds.conf.orig
-rm -f $FSDIR/usr/lib/lua/luci/view/admin_status/index.htm.orig
 
 >&2 echo "repacking squashfs..."
 rm -f "$IMG.new"
