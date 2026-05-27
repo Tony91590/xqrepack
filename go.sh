eb00d5d48d8db5b02d1936f42cf0562f32664188
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-05-27 19:26:23 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,45 +2,36 @@
 
 set -e
 
-echo "[1/4] Dropbear"
+echo " Dropbear"
 sed -i 's/channel=.*/channel="debug"/g' /etc/init.d/dropbear
 /etc/init.d/dropbear restart
 
-echo "[2/4] WiFi country + features"
-
-uci set wireless.wifi0.country='FR'
-uci set wireless.wifi1.country='FR'
-
-uci set wireless.@wifi-iface[0].bss_transition='1'
-uci set wireless.@wifi-iface[0].ieee80211k='1'
-uci set wireless.@wifi-iface[0].ieee80211v='1'
-
-uci set wireless.@wifi-iface[1].bss_transition='1'
-uci set wireless.@wifi-iface[1].ieee80211k='1'
-uci set wireless.@wifi-iface[1].ieee80211v='1'
-
-uci commit wireless
-
-echo "[3/4] TX power script"
-
-cat > /etc/init.d/wifi-tx-reg <<'EOF'
+cat > /etc/init.d/miwifi_overlay <<'EOF'
 #!/bin/sh /etc/rc.common
-START=99
-
-REGDOM="FR"
-TXPOWER_WL0=23
-TXPOWER_WL1=20
-
+START=00
+. /lib/functions/preinit.sh
 start() {
-    iw reg set "$REGDOM"
-    (sleep 60; iwconfig wl0 txpower "$TXPOWER_WL0"; iwconfig wl1 txpower "$TXPOWER_WL1")&
+        [ -e /data/overlay ] || mkdir /data/overlay
+        [ -e /data/overlay/upper ] || mkdir /data/overlay/upper
+        [ -e /data/overlay/work ] || mkdir /data/overlay/work
+
+        mount --bind /data/overlay /overlay
+        fopivot /overlay/upper /overlay/work /rom 1
+
+        #Fixup miwifi misc, and DO NOT use /overlay/upper/etc instead, /etc/uci-defaults/* may be already removed
+        /bin/mount -o noatime,move /rom/data /data 2>&-
+        /bin/mount -o noatime,move /rom/etc /etc 2>&-
+        /bin/mount -o noatime,move /rom/ini /ini 2>&-
+        /bin/mount -o noatime,move /rom/userdisk /userdisk 2>&-
+
+        return 0
 }
 EOF
 
-chmod +x /etc/init.d/wifi-tx-reg
-/etc/init.d/wifi-tx-reg enable
+chmod +x /etc/init.d/miwifi_overlay
 
-echo "[4/4] start service + reboot"
- /etc/init.d/wifi-tx-reg start
+/etc/init.d/miwifi_overlay enable
+
+sync
 
 reboot
