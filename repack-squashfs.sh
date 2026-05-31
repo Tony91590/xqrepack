81728e4620a232e73f31af210a8c595769fe005e
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-05-31 23:27:40 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -38,7 +38,7 @@ sed -i 's/channel=.*/channel=release2/' "$FSDIR/etc/init.d/dropbear"
 sed -i 's/flg_ssh=.*/flg_ssh=1/' "$FSDIR/etc/init.d/dropbear"
 
 # mark web footer so that users can confirm the right version has been flashed
-sed -i 's/romVersion%>/& xqrepack-adriano/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
+sed -i 's/romVersion%>/& xqrepack/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
 
 # stop resetting root password
 sed -i '/set_user(/a return 0' "$FSDIR/etc/init.d/system"
@@ -68,37 +68,10 @@ NVRAM
 # modify root password
 sed -i "s@root:[^:]*@root:${ROOTPW}@" "$FSDIR/etc/shadow"
 
-# stop phone-home in web UI
-cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
-(function(){ if (typeof window.MIWIFI_MONITOR !== "undefined") window.MIWIFI_MONITOR.log = function(a,b) {}; })();
-JS
-
 # add xqflash tool into firmware for easy upgrades
 cp xqflash "$FSDIR/sbin"
 chmod 0755      "$FSDIR/sbin/xqflash"
 chown root:root "$FSDIR/sbin/xqflash"
-
-# dont start crap services
-for SVC in stat_points statisticsservice \
-		datacenter \
-		xq_info_sync_mqtt \
-		xiaoqiang_sync \
-		plugincenter plugin_start_script.sh cp_preinstall_plugins.sh; do
-	rm -f $FSDIR/etc/rc.d/[SK]*$SVC
-done
-
-# prevent stats phone home & auto-update
-for f in StatPoints mtd_crash_log logupload.lua otapredownload; do > $FSDIR/usr/sbin/$f; done
-
-sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
-
-# cron jobs are mostly non-OpenWRT stuff
-for f in $FSDIR/etc/crontabs/*; do
-	sed -i 's/^/#/' $f
-done
-
-# as a last-ditch effort, change the *.miwifi.com hostnames to localhost
-sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
 
 # apply patch from xqrepack repository
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
