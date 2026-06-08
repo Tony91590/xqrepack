380a4a4394b6cd0a2c0abe4ea7522fbef42c8246
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-06-08 14:18:06 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -33,6 +33,7 @@ unsquashfs -f -d "$FSDIR" "$IMG"
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
 find patches -type f -name \*.orig -delete
 
+rm -f $FSDIR/etc/init.d/dropbear.orig
 rm -f $FSDIR/lib/wifi/qcawificfg80211.sh.orig
 rm -f $FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm.orig
 rm -f $FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html.orig
