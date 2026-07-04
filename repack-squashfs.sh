60cef80f18cc3f3df246afeec98d34de231a75d1
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-04 17:07:18 +0200

    Update force HTTPS script for EU region

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -30,8 +30,9 @@ unsquashfs -f -d "$FSDIR" "$IMG"
 
 cat > $FSDIR/etc/uci-defaults/enable_force_https.sh << EOF
 #!/bin/sh
-# for BSI certification, force https for all web access by default in UK region
-if [ "$(bdata get CountryCode)" = "UK" ]; then
+# for BSI certification, force https for all web access by default in EU region
+CountryCode=EU
+if [ "$CountryCode" = "UK" ]; then
 	uci set nginx.main.force_https=1
 	uci commit nginx
 fi
