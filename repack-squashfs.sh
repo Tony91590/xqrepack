173c83f71025c191eb26d3fa37429dc0e9aaa9cc
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-05 18:23:31 +0200

    Remove unnecessary blank line in repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -34,7 +34,6 @@ find patches -type f -name \*.orig -delete
 
 rm -f $FSDIR/etc/init.d/dropbear.orig
 
-
 >&2 echo "repacking squashfs..."
 rm -f "$IMG.new"
 mksquashfs "$FSDIR" "$IMG.new" -comp xz -b 256K -no-xattrs
