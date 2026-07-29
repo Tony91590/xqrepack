d6f6d135d2aaf5372eae08273ea9689ef61a7a46
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-29 18:13:57 +0200

    Update ubinize.sh

--- ubinize.sh
+++ ubinize.sh
@@ -36,7 +36,6 @@ image=$KERNEL
 vol_id=0
 vol_type=dynamic
 vol_name=kernel
-vol_alignment=1
 
 [rootfs]
 mode=ubi
@@ -44,7 +43,6 @@ image=$ROOTFS
 vol_id=1
 vol_type=dynamic
 vol_name=rootfs
-vol_alignment=1
 CFGEND
 
 # generate an empty rootfs_data volume if requested
@@ -58,6 +56,12 @@ vol_name=rootfs_data
 vol_flags=autoresize
 CFGEND2
 
-sudo ubinize -o "$OUTPUT" -m 2048 -p 128KiB -O 2048 "$UBICFG"
+ubinize \
+    -m 2048 \
+    -p 128KiB \
+    -o "$OUTPUT" \
+    "$UBICFG"
+
+truncate -s 35651584 "$OUTPUT"
 
 echo "done."
